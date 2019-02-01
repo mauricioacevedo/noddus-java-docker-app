b1d98f9757fd30a8bcc31dc9fa4e363368107b47
Mauricio Acevedo <mauricio.acevedo@gmail.com>
2019-02-01 01:02:52 -0500

    More improvements on build.sh file.

--- build.sh
+++ build.sh
@@ -1,15 +1,19 @@
 #!/bin/bash
 #
-echo  "Noddus java+docker Use Case build and run script.\n"
-
+echo "#######################################################"
+echo "#######################################################"
+echo "## Noddus java+docker Use Case build and run script. ##"
+echo "#######################################################"
+sleep 2
 #
 # build projects
 ### backend
-echo "[START] backend build.\n"
-/usr/bin/mvn package -f ./spring-protobuf-server/
+echo "[START] backend build"
+/usr/bin/mvn package -f ./spring-protobuf-server/pom.xml
 
 ### frontend [OPTIONAL NODEJS REQUIRED!!!]
 # echo "[START] frontend build"
+# sleep 2
 ## LOAD nodejs dependencies
 # /usr/bin/npm install --prefix ./protobuf-frontend
 ## BUILD frontend
@@ -18,14 +22,15 @@ echo "[START] backend build.\n"
 
 ##build images
 echo "[START] build docker images.\n"
-/usr/bin/docker build -t spring-protobuf-server ./spring-protobuf-server/Dockerfile
-/usr/bin/docker build -t protobuf-frontend ./protobuf-frontend/Dockerfile
+sleep 2
+/usr/bin/docker build -t spring-protobuf-server ./spring-protobuf-server/
+/usr/bin/docker build -t protobuf-frontend ./protobuf-frontend/
 
 ## create a shared network
 /usr/bin/docker network create -d bridge proto-network
 
 echo "[RUN] protobuf backend and frontend..\n"
-
+sleep 2
 ## run containers
 /usr/bin/docker run -d --rm -p 8080:8080 --network proto-network -h backend --name backend spring-protobuf-server
 /usr/bin/docker run -d --rm -p 80:80 --network proto-network -h frontend --name frontend protobuf-frontend
